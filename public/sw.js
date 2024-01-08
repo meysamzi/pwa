d3696c662850025c3fc4053ef34c22846d7569c3
meysamzi <meysamzeynali2@gmail.com>
2024-01-08 12:19:11 +0330

    feat: putting fetch middle ware to intercept requests

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,40 @@
 const cacheName = "firstVersion";
-const cacheList = ["./", "./index.html", "./main.css", "./main.js"];
+const cacheList = ["/", "/index.html", "static/js/bundle.js"];
 
 self.addEventListener("install", (ev) => {
   ev.waitUntil(
     caches.open(cacheName).then((cache) => {
-      cache.addAll(cacheList);
+      return cache.addAll(cacheList).then(() => self.skipWaiting());
     })
   );
+});
+
+self.addEventListener("activate", (ev) => {
+  ev.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  if (navigator.onLine) {
+    console.log(0);
+    let fetchRequest = e.request;
+    return fetch(fetchRequest).then((response) => {
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
+      }
+      let responseToCache = response.clone();
+      caches.open(cacheName).then((cache) => {
+        cache.put(e.request, responseToCache);
+      });
+      return response;
+    });
+  } else {
+    console.log(1);
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        if (response) {
+          return response;
+        }
+      })
+    );
+  }
 });
